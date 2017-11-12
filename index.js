4faf20d1a1534dfc8a66449c00550a502bcbe9ad
Ryan Lee <ryanlee91@gmail.com>
2017-11-12 20:26:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ function BoardMember(name, homeState, training){
   this.homeState = homeState;
   this.training = training;
   this.veto = function(){
-    console.log("No, I must disagree");
+    return "No, I must disagree";
   }
   this.approve = function(){
-    returns "You can do that!";
+    return "You can do that!";
   }
   this.doCharity = function(){
-    returns "I like to help people.";
+    return "I like to help people.";
   }
   this.releasePressStatement = function(){
-    returns "You will see great things from Scuber.";
+    return "You will see great things from Scuber.";
   }
   this.sayHi = function(){
-    returns `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
   }
 }
 
